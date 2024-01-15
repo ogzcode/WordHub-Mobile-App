d48a2da36c0d3b70c3d205e902b42d4104f9fbaa
ogzCode <oguzguc44@gmail.com>
2024-01-15 10:32:09 +0300

    Add style-related modules

---
+++ src/style/border.js
@@ -0,0 +1,27 @@
+export const border = {
+    "rounded": {
+        "none": 0,
+        "sm": 2,
+        "md": 6,
+        "lg": 8,
+        "xl": 12,
+        "2xl": 16,
+        "3xl": 24,
+        "full": "50%"
+      },
+    "width": {
+        "0": 0,
+        "2": 2,
+        "4": 4,
+        "8": 8,
+        "default": 1
+    },
+    "style": {
+        "solid": "solid",
+        "dashed": "dashed",
+        "dotted": "dotted",
+        "double": "double",
+        "hidden": "hidden",
+        "none": "none"
+      }
+}
